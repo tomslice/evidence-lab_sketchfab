d32098daeb280bac3df8db41945e918e43a367dc
tomslice <105735581+tomslice@users.noreply.github.com>
2022-09-01 11:54:31 +0200

    Update sketchfabBoilerplate_appname.js

--- js/sketchfabBoilerplate_appname.js
+++ js/sketchfabBoilerplate_appname.js
@@ -18,8 +18,8 @@ client.init(uid, {
   ui_animations: 0, // Usage: Setting to 0 will hide the animation menu and timeline.
   ui_annotations: 0, // Usage: Setting to 0 will hide the Annotation menu.
   ui_controls: 0, // Usage: Setting to 0 will hide all the viewer controls at the bottom of the viewer (Help, Settings, Inspector, VR, Fullscreen, Annotations, and Animations).
-  ui_fullscreen: 1, // Usage: Setting to 0 will hide the Fullscreen button.
-  ui_general_controls: 1, // Usage: Setting to 0 will hide main control buttons in the bottom right of the viewer (Help, Settings, Inspector, VR, Fullscreen).
+  ui_fullscreen: 0, // Usage: Setting to 0 will hide the Fullscreen button.
+  ui_general_controls: 0, // Usage: Setting to 0 will hide main control buttons in the bottom right of the viewer (Help, Settings, Inspector, VR, Fullscreen).
   ui_help: 0, // Usage: Setting to 0 will hide the Help button.
   ui_hint: 1, // Usage: Setting to 0 will always hide the viewer hint animation ("click & hold to rotate"). Setting to 1 will show the hint the first time per browser session (using a cookie). Setting to 2 will always show the hint.
   ui_infos: 0, // Usage: Setting to 0 will hide the model info bar at the top of the viewer.
